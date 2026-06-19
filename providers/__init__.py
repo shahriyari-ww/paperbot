7b0fb6d05f734964f7082bc5dfc9b16c4e30443f
shahriyari-ww <shahriyari.ww@gmail.com>
2026-06-19 23:08:54 +0330

    Update __init__.py

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,2 +1,10 @@
- 
-# فایل خالی
+# providers/__init__.py
+from .arxiv_provider import search_arxiv
+from .pubmed_provider import search_pubmed, search_pubmed_advanced
+from .crossref_provider import search_crossref
+from .unpaywall_provider import search_unpaywall
+from .semantic_scholar_provider import search_semantic_scholar
+from .core_provider import search_core
+from .base_provider import search_base
+from .doaj_provider import search_doaj
+from .scihub_provider import search_scihub
